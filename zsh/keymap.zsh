2e36b0179a6f3e16d48bde6405ba6bf86ea2cd10
Isak Berg Endresen <isakbendresen@gmail.com>
2024-06-27 15:42:39 +0200

    Update zsh keymap

--- zsh/keymap.zsh
+++ zsh/keymap.zsh
@@ -6,9 +6,9 @@ bindkey "^H" backward-char
 bindkey "^J" down-line-or-beginning-search
 bindkey "^K" up-line-or-beginning-search
 bindkey "^L" forward-char
-bindkey "^[i" beginning-of-line
-bindkey "^[a" end-of-line
-bindkey "^D" kill-line
-bindkey "^Xd" kill-whole-line
+bindkey "^_" beginning-of-line
+bindkey "^A" end-of-line
+# bindkey "^D" kill-line
+bindkey "^D" kill-whole-line
 bindkey "^U" undo
 
